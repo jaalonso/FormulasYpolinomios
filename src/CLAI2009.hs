92a1b88db4e78d327f7c2dbd093570093d1b10b3
Jose A. Alonso <JoseA.Alonso@gmail.com>
2017-07-17 09:01:19 +0200

    Transformaciones entre fórmulas y polinomios

--- src/CLAI2009.hs
+++ src/CLAI2009.hs
@@ -814,59 +814,98 @@ prop_deriv_prod :: Polinomio -> Polinomio -> Variable -> Bool
 prop_deriv_prod p q x = deriv (p*q) x == (deriv p x)*q + p*(deriv q x) 
 
 -- ---------------------------------------------------------------------
+-- * Transformaciones entre fórmulas y polinomios
+-- ---------------------------------------------------------------------
+
+-- ---------------------------------------------------------------------
 -- ** Transformación de proposiciones en polinomios
 -- ---------------------------------------------------------------------
 
--- (tr f) es el polinomio correspondiente a la fórmula f.
+-- | (tr f) es el polinomio correspondiente a la fórmula f. Por ejemplo,
+--
+-- >>> tr (p ∧ (q ∨ r))
+-- p*q+p*q*r+p*r
+-- >>> tr (p → p ∨ q)
+-- 1
+-- >>> tr ((p → q) ∨ (q → p))
+-- 1
+-- >>> tr ((p → q) ∨ (q → r))
+-- 1
+-- >>> tr ((p → q) ∨ (r → q))
+-- 1+p*q*r+p*r
 tr :: FProp -> Polinomio
 tr T          = uno
 tr F          = cero
-tr (Atom p)   = P [M [p]]
-tr (Neg p)    = uno + (tr p)
-tr (Conj a b) = (tr a) * (tr b)
-tr (Disj a b) = (tr a) + (tr b) + ((tr a) * (tr b))
-tr (Impl a b) = uno + ((tr a) + ((tr a) * (tr b)))
-tr (Equi a b) = uno + ((tr a) + (tr b))
+tr (Atom a)   = P [M [a]]
+tr (Neg a)    = uno + tr a
+tr (Conj a b) = tr a * tr b
+tr (Disj a b) = tr a + tr b + tr a * tr b
+tr (Impl a b) = uno + tr a + tr a * tr b
+tr (Equi a b) = uno + tr a + tr b
 
 pro_tr_bien_definida :: FProp -> Bool
 pro_tr_bien_definida f = esPolinomio (tr f)
 
 -- ---------------------------------------------------------------------
--- * Transformaciones entre polinomios y fórmulas
--- ---------------------------------------------------------------------
-
--- ---------------------------------------------------------------------
 -- ** Transformación de polinomios en proposiciones
 -- ---------------------------------------------------------------------
 
--- (theta2 m) es la fórmula correspondiente al polinomio p según las
+-- | (theta2 m) es la fórmula correspondiente al polinomio p según las
 -- siguientes reglas:
---    theta 0     = F
---    theta (a+b) = no ((theta a) ↔ (theta b))
+-- 
+-- * theta 0     = F
+-- * theta (a+b) = no ((theta a) ↔ (theta b))
+-- 
+-- Por ejemplo,
+--
+-- >>> theta (P [mUno])
+-- ⊤
+-- >>> theta (P [mUno, M ["p"]])
+-- ¬(⊤ ↔ p)
+-- >>> theta (P [mUno, M ["p"], M ["p","q"]])
+-- ¬(⊤ ↔ ¬(p ↔ (p ∧ q)))
+-- >>> theta (P [mUno, M ["p"], M ["p","q"],M["p","q","r"]])
+-- ¬(⊤ ↔ ¬(p ↔ ¬((p ∧ q) ↔ (p ∧ (q ∧ r)))))
+-- >>> theta (P [mUno, M ["p"], M ["p","q"],M["p","q","r"],M["r"]])
+-- ¬(⊤ ↔ ¬(p ↔ ¬((p ∧ q) ↔ ¬((p ∧ (q ∧ r)) ↔ r))))
 theta :: Polinomio -> FProp
 theta (P ms) = thetaAux ms
+  where thetaAux :: [Monomio] -> FProp
+        thetaAux []     = F
+        thetaAux [m]    = theta2 m
+        thetaAux (m:ms) = no ((theta2 m) ↔ (theta (P ms)))
 
-thetaAux :: [Monomio] -> FProp
-thetaAux []     = F
-thetaAux [m]    = theta2 m
-thetaAux (m:ms) = no ((theta2 m) ↔ (theta (P ms)))
-
--- (theta2 m) es la fórmula correspondiente al monomio m según las
+-- | (theta2 m) es la fórmula correspondiente al monomio m según las
 -- siguientes reglas:
---    theta2 1     = T
---    theta2 (x_i) = p_i
---    theta2 (a*b) = (theta2 a) ∧ (theta2 b)
+-- 
+-- * theta2 1     = T
+-- * theta2 (x_i) = p_i
+-- * theta2 (a*b) = (theta2 a) ∧ (theta2 b)
+-- 
+-- Por ejemplo,
+--
+-- >>> theta2 (M ["p","q","r"])
+-- (p ∧ (q ∧ r))
 theta2 :: Monomio -> FProp
 theta2 (M vs) = theta2Aux vs
+  where theta2Aux :: [SimboloProposicional] -> FProp
+        theta2Aux []     = T
+        theta2Aux [v]    = Atom v
+        theta2Aux (v:vs) = Conj (Atom v) (theta2Aux vs)
 
-theta2Aux :: [SimboloProposicional] -> FProp
-theta2Aux []     = T
-theta2Aux [v]    = Atom v
-theta2Aux (v:vs) = Conj (Atom v) (theta2Aux vs)
-
+-- | Comprueba que para cualquier fórmula f, (theta (tr f)) es
+-- equivalente a f.
+--
+-- >>> quickCheck prop_theta_tr
+-- +++ OK, passed 100 tests.
 prop_theta_tr :: FProp -> Bool
-prop_theta_tr p = equivalentes (theta (tr p)) p
+prop_theta_tr f = equivalentes (theta (tr f)) f
 
+-- | Comprueba que, para cualquier polinomio p, tr (theta p) es igual a
+-- p. 
+--
+-- >>> quickCheck prop_tr_theta
+-- +++ OK, passed 100 tests.
 prop_tr_theta :: Polinomio -> Bool
 prop_tr_theta p = tr (theta p) == p
 
